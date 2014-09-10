ff160650361ff9c026255d8a7045bc16e455cd46
Mike Munhall <munhall.mike@gmail.com>
2014-09-10 09:11:14 -0600

    Added mocha to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 /*global gulp:true, require:true */
 
 var gulp = require('gulp'),
-    browserify = require('gulp-browserify');
+    browserify = require('gulp-browserify'),
+    mocha = require('gulp-mocha');
 
 gulp.task('default', function () {
+    "use strict";
+
     // place code for your default task here
 });
 
-gulp.task('scripts', function () {
+gulp.task('test', function () {
+    "use strict";
+
+    return gulp.src('test/test.js', {read: false})
+        .pipe(mocha({reporter: 'spec'}));
+});
+
+gulp.task('browserify', function () {
+    "use strict";
+
     gulp.src('dumb-cache.js')
         .pipe(browserify({
             insertGlobals : true,
