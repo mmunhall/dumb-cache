7974833c7b3df04fd8d309d169410e16601b088a
Mike Munhall <munhall.mike@gmail.com>
2014-09-10 08:50:18 -0600

    Added gulp and gulp-browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
-/*global require:true */
+/*global gulp:true, require:true */
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify');
 
 gulp.task('default', function () {
     // place code for your default task here
+});
+
+gulp.task('scripts', function () {
+    gulp.src('dumb-cache.js')
+        .pipe(browserify({
+            insertGlobals : true,
+            debug : !gulp.env.production
+        }))
+        .pipe(gulp.dest('./build/js'));
 });
